250a8908caec10c52cfcea835792e84cf80f03a3
pacostas0102 <pacostas0102@github.com>
2022-10-10 23:06:55 -0500

    ToolbarACT

---
+++ Activities/Acitivities_Toolbar/app/src/main/java/com/example/class21092022/AyudaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.class21092022
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AyudaActivity:AppCompatActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ayuda)
+    }
+}
---
+++ Activities/Acitivities_Toolbar/app/src/main/java/com/example/class21092022/FavoritosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.class21092022
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FavoritosActivity :AppCompatActivity(){
+    public override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.favoritos)
+    }
+}
